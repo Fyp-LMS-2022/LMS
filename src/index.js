33d4f838327d8c6cec703bb6d5069255c18e80ec
Fyp-LMS-2022 <fyplms2021@gmail.com>
2021-11-02 16:13:29 +0500

    admin student updated

--- src/index.js
+++ src/index.js
@@ -1,88 +1,88 @@
-google.charts.load('current', { 'packages': ['corechart'] });
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.load('current', { 'packages': ['corechart'] });
+// google.charts.setOnLoadCallback(drawChart);
 
 
-/////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-////////////////////////////////////////////////
-//////////// AdminDashboard top-Counters
-
-
-// const counter = document.querySelectorAll('.counter')
-// counter.forEach(counter = () {
-//     const target = counter.getAttribute('data-target')
-
-// })
+// /////////////////////////////////////////////////////////////////////////////////////////
 
 
 
+// ////////////////////////////////////////////////
+// //////////// AdminDashboard top-Counters
 
 
-////////////////////////////////////////////////
-//////////// AdminDashboard graphs
+// // const counter = document.querySelectorAll('.counter')
+// // counter.forEach(counter = () {
+// //     const target = counter.getAttribute('data-target')
 
-function drawChart() {
-
-    var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Ahmed', 11],
-        ['Eat', 2],
-        ['Commute', 2],
-        ['Watch TV', 2],
-        ['Sleep', 7]
-    ]);
-
-    var options = {
-        title: 'My Daily Activities'
-    };
-
-    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-    chart.draw(data, options);
-}
-
-function drawChart1() {
-
-    var data1 = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Ahmed', 11],
-        ['Eat', 2],
-        ['Commute', 2],
-        ['Watch TV', 2],
-        ['Sleep', 7]
-    ]);
-
-    var options1 = {
-        title: 'My Daily Activities'
-    };
-
-    var chart1 = new google.visualization.PieChart(document.getElementById('piechart1'));
-
-    chart1.draw(data1, options1);
-}
+// // })
 
 
-function drawChart() {
 
-    var data = google.visualization.arrayToDataTable([
-        ['Task', 'Hours per Day'],
-        ['Ahmed', 11],
-        ['Eat', 2],
-        ['Commute', 2],
-        ['Watch TV', 2],
-        ['Sleep', 7]
-    ]);
 
-    var options = {
-        title: 'My Daily Activities'
-    };
 
-    var chart = new google.visualization.PieChart(document.getElementById('piechart2'));
+// ////////////////////////////////////////////////
+// //////////// AdminDashboard graphs
 
-    chart.draw(data, options);
-}
+// function drawChart() {
+
+//     var data = google.visualization.arrayToDataTable([
+//         ['Task', 'Hours per Day'],
+//         ['Ahmed', 11],
+//         ['Eat', 2],
+//         ['Commute', 2],
+//         ['Watch TV', 2],
+//         ['Sleep', 7]
+//     ]);
+
+//     var options = {
+//         title: 'My Daily Activities'
+//     };
+
+//     var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+//     chart.draw(data, options);
+// }
+
+// function drawChart1() {
+
+//     var data1 = google.visualization.arrayToDataTable([
+//         ['Task', 'Hours per Day'],
+//         ['Ahmed', 11],
+//         ['Eat', 2],
+//         ['Commute', 2],
+//         ['Watch TV', 2],
+//         ['Sleep', 7]
+//     ]);
+
+//     var options1 = {
+//         title: 'My Daily Activities'
+//     };
+
+//     var chart1 = new google.visualization.PieChart(document.getElementById('piechart1'));
+
+//     chart1.draw(data1, options1);
+// }
+
+
+// function drawChart() {
+
+//     var data = google.visualization.arrayToDataTable([
+//         ['Task', 'Hours per Day'],
+//         ['Ahmed', 11],
+//         ['Eat', 2],
+//         ['Commute', 2],
+//         ['Watch TV', 2],
+//         ['Sleep', 7]
+//     ]);
+
+//     var options = {
+//         title: 'My Daily Activities'
+//     };
+
+//     var chart = new google.visualization.PieChart(document.getElementById('piechart2'));
+
+//     chart.draw(data, options);
+// }
 
 
 ////////////////////////////////////////
@@ -92,7 +92,7 @@ function drawChart() {
 // var form = document.getElementById('teacherSubmitBUtton');
 // form.addEventListener()
 
-function onFormSubmit() {
+function onNewTeacherFormSubmit() {
 
     var retreivedFormData = retreiveNewTeacherData();
     newTeacherRecord(retreivedFormData);
@@ -101,10 +101,11 @@ function onFormSubmit() {
 function retreiveNewTeacherData() {
     var newTeacherData = {};
     newTeacherData["newTeacherName"] = document.getElementById("newTeacherName").value;
-    newTeacherData["teacherProgram"] = document.getElementById("teacherProgram").value;
-    newTeacherData["teacherQualification"] = document.getElementById("teacherQualification").value;
-    newTeacherData["teacherCourse"] = document.getElementById("teacherCourse").value;
-    newTeacherData["teacherEmail"] = document.getElementById("teacherEmail").value;
+    newTeacherData["newteacherUsername"] = document.getElementById("newteacherUsername").value;
+    newTeacherData["newteacherPassword"] = document.getElementById("newteacherPassword").value;
+    newTeacherData["newteacherProgram"] = document.getElementById("newteacherProgram").value;
+    newTeacherData["newteacherCourseCode"] = document.getElementById("newteacherCourseCode").value;
+
 
     return newTeacherData;
 
@@ -114,19 +115,125 @@ function retreiveNewTeacherData() {
 function newTeacherRecord(data) {
     var table = document.getElementById("teachersList").getElementsByTagName("tbody")[0];
     var newRow = table.insertRow(table.length);
+    // cell0 = newRow.insertCell(0);
+    // cell0.innerHTML = data.newTeacherName;
+    // cell1 = newRow.insertCell(1);
+    // cell1.innerHTML = data.newteacherUsername;
+    // cell2 = newRow.insertCell(2);
+    // cell2.innerHTML = data.newteacherPassword;
+    // cell3 = newRow.insertCell(3);
+    // cell3.innerHTML = data.newteacherProgram;
+    // cell4 = newRow.insertCell(4);
+    // cell4.innerHTML = data.newteacherCourseCode;
+    // cell5 = newRow.insertCell(5);
+    // cell5.innerHTML = `<a>Del</a>`
+
+
+    table.innerHTML += `
+    <tr>
+    <td>${data.newTeacherName}</td>
+    <td><button class="btn btn-success ">Assign Course</button></td>
+    </tr>`;
+
+
+    // cell1 = newRow.insertCell(6);
+    // cell1.innerHTML = `<a>del</a>`;
+}
+
+
+
+
+////////////////////////////////////////////
+/////////// FOR STUDENT COURSES--- Changing the values of dropdown list reflecting the checked radio buttons in HTML
+
+
+
+function onNewStudentFormSubmit() {
+
+    var retreivedStudentFormData = retreiveNewStudentData();
+    newStudentRecord(retreivedStudentFormData);
+}
+
+function retreiveNewStudentData() {
+    var newStudentData = {};
+    newStudentData["newStudentName"] = document.getElementById("addStudent-Name").value;
+    newStudentData["newStudentEmail"] = document.getElementById("addStudent-email").value;
+    newStudentData["newStudentEnrollment"] = document.getElementById("addStudent-enrollment").value;
+    newStudentData["newStudentPassword"] = document.getElementById("addStudent-Password").value;
+    newStudentData["newStudentSemester"] = document.getElementById("addStudent-semester").value;
+    newStudentData["newStudentProgram"] = document.getElementById("addStudent-program").value;
+
+
+    return newStudentData;
+
+}
+
+
+function newStudentRecord(data) {
+    var table = document.getElementById("adminStudentsTable").getElementsByTagName("tbody")[0];
+    var newRow = table.insertRow(table.length);
     cell0 = newRow.insertCell(0);
-    cell0.innerHTML = data.newTeacherName;
+    cell0.innerHTML = data.newStudentName;
     cell1 = newRow.insertCell(1);
-    cell1.innerHTML = data.teacherProgram;
+    cell1.innerHTML = data.newStudentEmail;
     cell2 = newRow.insertCell(2);
-    cell2.innerHTML = data.teacherQualification;
+    cell2.innerHTML = data.newStudentEnrollment;
     cell3 = newRow.insertCell(3);
-    cell3.innerHTML = data.teacherCourse;
+    cell3.innerHTML = data.newStudentPassword;
     cell4 = newRow.insertCell(4);
-    cell4.innerHTML = data.teacherEmail;
+    cell4.innerHTML = data.newStudentSemester;
     cell5 = newRow.insertCell(5);
-    cell5.innerHTML = "<a>Edit</a>";
-    cell5.pro
-        // cell1 = newRow.insertCell(6);
-        // cell1.innerHTML = `<a>del</a>`;
+    cell5.innerHTML = data.newStudentProgram;
+    cell6 = newRow.insertCell(6);
+    cell6.innerHTML = `<button class="btn btn-success" data-bs-target="#exampleModal" data-bs-toggle="modal" onClick="assignStudentCourse(this)">Edit</button> `;
+    cell7 = newRow.insertCell(7);
+    cell7.innerHTML = `<button class="btn btn-success" >Delete</button> `;
+
+
+
+
+    // table.innerHTML += `
+    // <tr>
+    // <td>${data.newStudentName}</td>
+    // <td>${data.newStudentEmail}</td>
+    // <td>${data.newStudentEnrollment}</td>
+    // <td>${data.newStudentPassword}</td>
+    // <td>${data.newStudentSemester}</td>
+    // <td>${data.newStudentProgram}</td>
+    // <td><button  class="btn btn-success " data-bs-target="#courseAssignModal" data-bs-toggle="modal" onclick="assignStudentCourse(this)">Assign Course</button></td>
+    // </tr>`;
+
+
+    // cell1 = newRow.insertCell(6);
+    // cell1.innerHTML = `<a>del</a>`;
+}
+
+
+////////////////////////////////////////////////////////////////////
+//////////////// assignStudentCourse
+
+
+
+function assignStudentCourse(td) {
+    selectRow = td.parentElement.parentElement;
+    console.log(selectRow);
+
+
+
+
+
+    document.getElementById("addStudent-Name").value = selectRow.getElementsByTagName('td')[0].innerHTML;
+
+    document.getElementById("addStudent-email").value = selectRow.getElementsByTagName('td')[1].innerHTML;
+
+    document.getElementById("addStudent-enrollment").value = selectRow.getElementsByTagName('td')[2].innerHTML;
+
+    document.getElementById("addStudent-Password").value = selectRow.getElementsByTagName('td')[3].innerHTML;
+
+    document.getElementById("addStudent-semester").value = selectRow.getElementsByTagName('td')[4].innerHTML;
+
+    document.getElementById("addStudent-program").value = selectRow.getElementsByTagName('td')[5].innerHTML;
+
+
+
 }
